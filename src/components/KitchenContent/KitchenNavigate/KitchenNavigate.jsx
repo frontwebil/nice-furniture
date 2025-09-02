cb92d2020e1c33a4be7f9efcd177169d88a7e03f
iLyhaha <gollfpd@gmail.com>
2025-09-02 16:40:17 +0300

    update KitchenPage

--- src/components/KitchenContent/KitchenNavigate/KitchenNavigate.jsx
+++ src/components/KitchenContent/KitchenNavigate/KitchenNavigate.jsx
@@ -1,9 +1,11 @@
+import { FaLongArrowAltLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export function KitchenNavigate({ title }) {
   return (
-    <div className="kitchen-navigate text-xxs">
-      <Link to={"/"}>Файні меблі / Кухні</Link> / <span className="font-semiBold">{title}</span> 
-    </div>
+    <Link to={'/'} className="kitchen-navigate text-sm">
+      <FaLongArrowAltLeft />
+      На Головну
+    </Link>
   );
 }
