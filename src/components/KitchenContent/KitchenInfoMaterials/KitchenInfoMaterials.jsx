c25578044e67bd63c21c503c6236833c2084c779
iLyhaha <gollfpd@gmail.com>
2025-09-08 12:41:58 +0300

    updateInfo

--- src/components/KitchenContent/KitchenInfoMaterials/KitchenInfoMaterials.jsx
+++ src/components/KitchenContent/KitchenInfoMaterials/KitchenInfoMaterials.jsx
@@ -4,22 +4,34 @@ export function KitchenInfoMaterials({ currentKitchen }) {
   return (
     <div className="kitchen-info-materials">
       <div className="kitchen-info-materials-column">
-        <div className="kitchen-info-materials-column-title text-xs">Материалы</div>
-        <p className="gray text-xs" style={{ whiteSpace: "pre-wrap" }}>
-          {currentKitchen.materials}
-        </p>
+        <div className="kitchen-info-materials-column-title text-xs">
+          Материалы
+        </div>
+        <p
+          className="gray text-xs"
+          style={{ whiteSpace: "pre-wrap" }}
+          dangerouslySetInnerHTML={{ __html: currentKitchen.materials }}
+        />
       </div>
       <div className="kitchen-info-materials-column">
-        <div className="kitchen-info-materials-column-title text-xs">Фурнитура</div>
-        <p className="gray text-xs" style={{ whiteSpace: "pre-wrap" }}>
-          {currentKitchen.furniture}
-        </p>
+        <div className="kitchen-info-materials-column-title text-xs">
+          Фурнитура
+        </div>
+        <p
+          className="gray text-xs"
+          style={{ whiteSpace: "pre-wrap" }}
+          dangerouslySetInnerHTML={{ __html: currentKitchen.furniture }}
+        />
       </div>
       <div className="kitchen-info-materials-column">
-        <div className="kitchen-info-materials-column-title text-xs">Цвета кромки фасадов</div>
-        <p className="gray text-xs" style={{ whiteSpace: "pre-wrap" }}>
-          {currentKitchen.colorEdgesFacad}
-        </p>
+        <div className="kitchen-info-materials-column-title text-xs">
+          Цвета кромки фасадов
+        </div>
+        <p
+          className="gray text-xs"
+          style={{ whiteSpace: "pre-wrap" }}
+          dangerouslySetInnerHTML={{ __html: currentKitchen.colorEdgesFacad }}
+        />
       </div>
     </div>
   );
