a2e346497137e73684f1d981966cb946a29cc6b5
iLyhaha <gollfpd@gmail.com>
2026-05-08 17:08:21 +0300

    update

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -17,11 +17,11 @@ export function MainPage() {
       <Hero />
       <Catalog />
       <SeeMore />
-      <Advantages />
-      <HowBornKitchen />
       <Testimonials />
       <Discount />
+      <HowBornKitchen />
       <CalculationQuiz />
+      <Advantages />
       <AboutUs />
       <Partners />
       <Questions />
